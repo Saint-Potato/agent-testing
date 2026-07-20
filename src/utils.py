4d62d2c736e411aa00eb145cd78f18ee0f3e7a6d
tj20041 <tanmay20041@gmail.com>
2026-07-20 11:46:52 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -2,12 +2,11 @@ import json
 
 def parse_records(records):
     results = []
+
     for row in records:
-        try:
-            results.append({
-                'id': row[0],
-                'data': json.loads(row[1]) if len(row) > 1 else {}
-            })
-        except Exception:
-            continue
+        results.append({
+            "id": row[0],
+            "data": json.loads(row[1])   # Exception is no longer handled
+        })
+
     return results
